e28191275d177b1ffb502114d292355253578184
FaezehSalehi <faezehsalehi-ac>
2026-08-08 14:33:11 +0330

    feat : add two_sum task

---
+++ tasks/two_sum.py
@@ -0,0 +1,15 @@
+def solve (nums: list, target: int) -> list:
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num]= i
+    return []
+
+
+TEST_CASES = [
+    {"input" : {"nums":[2,7,11,15], "target":9}},
+    {"input" : {"nums":[3,2,4], "target":6}},
+   
+]
